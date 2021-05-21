648f076da4129946fb783a34391f219575d7ce5b
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-05-21 00:17:16 -0300

    update

---
+++ config/script.js
@@ -0,0 +1,12 @@
+var asideMenu = document.querySelector("aside")
+
+
+function closeMenu(){
+    asideMenu.style.display = "none"
+}
+
+function menuToggle(){
+    asideMenu.style.display = "block"
+}
+
+
---
+++ paginas/videos.js
@@ -0,0 +1,15 @@
+function openMusic() {
+    document.querySelector("aside").style.display = "block"
+}
+
+//Musicas
+
+function addMusicHay() {
+    document.querySelector("#wanna-be").style.display = "none"
+    document.querySelector("#hayloft").style.display = "block"
+}
+
+function addMusicWanna() {
+    document.querySelector("#hayloft").style.display = "none"
+    document.querySelector("#wanna-be").style.display = "block"
+}
